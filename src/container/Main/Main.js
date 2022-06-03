b9d5ba497f31bccfc6b0ff80a9b8792139b347b1
Yura <pumpkteam3@gmail.com>
2022-06-04 02:25:18 +0300

    added article cards

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Line } from 'components/Line/Line'
+import './Main.scss'
+import { NewsItem } from 'components/News/NewsItem'
+import { Grid } from '@mui/material'
+import news from 'utils/newsArray'
 
 export const Main = () => {
     return (
         <>
-            <h1>Main</h1>
+            <main>
+                <div className="content__header">
+                    <Line />
+                    <div className="content__header-title">
+                        <h3>News</h3>
+                    </div>
+                </div>
+
+                <Grid
+                    container
+                    spacing={2}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    {news.map((article) => (
+                        <Grid item xs={12} sm={6} md={4}>
+                            <NewsItem description="fgsddsg" date="33513" />
+                        </Grid>
+                    ))}
+                </Grid>
+            </main>
         </>
     )
 }
