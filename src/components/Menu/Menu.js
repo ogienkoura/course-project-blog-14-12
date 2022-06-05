d1c187f9e9184fe040f099ab67e9b61ec2832c18
Yura <pumpkteam3@gmail.com>
2022-06-05 11:02:11 +0300

    Добавил заглушки в ссылки

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,22 +5,22 @@ export const Menu = () => {
     return (
         <ul className="menu">
             <li className="menu__item">
-                <a href="#">Home</a>
+                <a href="#/">Home</a>
             </li>
             <li className="menu__item">
-                <a href="#">News</a>
+                <a href="#/">News</a>
             </li>
             <li className="menu__item">
-                <a href="#">Lifestyle</a>
+                <a href="#/">Lifestyle</a>
             </li>
             <li className="menu__item">
-                <a href="#">Places</a>
+                <a href="#/">Places</a>
             </li>
             <li className="menu__item">
-                <a href="#">About Us</a>
+                <a href="#/">About Us</a>
             </li>
             <li className="menu__item">
-                <a href="#">Contact</a>
+                <a href="#/">Contact</a>
             </li>
         </ul>
     )
